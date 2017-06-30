f9147742439f540b75e8fe8e45083e68a01192bd
LmLilly <lillym@Lidiettes-MacBook-Pro.local>
2017-06-29 19:29:39 -0700

    Cloze deletion flashcard

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,7 +1,12 @@
 "use strict";
 (function(){
+    function ClozeCard(fullText, cloze){
+        this.cloze = cloze;
+        this.partial = fullText.replace(cloze, "... ");
+        this.fullText = fullText;
+    }
     module.exports = ClozeCard;
-    
+
 })();
 
 
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 "use strict";
 (function(){
-     var BasicCard = require("./BasicCard");
-    // var ClozeCard = require("./ClozeCard");
+  		var BasicCard = require("./BasicCard");
+    	var ClozeCard = require("./ClozeCard");
      
 var firstPresident = new BasicCard(
     "Who was the first president of the United States?", "George Washington");
 
 // "Who was the first president of the United States?"
-console.log(firstPresident.front); 
+//console.log(firstPresident.front); 
 
 // "George Washington"
-console.log(firstPresident.back); 
-/*
+//console.log(firstPresident.back); 
+
 var firstPresidentCloze = new ClozeCard(
     "George Washington was the first president of the United States.", "George Washington");
 
@@ -19,11 +19,11 @@ var firstPresidentCloze = new ClozeCard(
 console.log(firstPresidentCloze.cloze); 
 
 // " ... was the first president of the United States.
-console.log(firstPresidentCloze.partial); "
+console.log(firstPresidentCloze.partial); 
 
 // "George Washington was the first president of the United States.
-console.log(firstPresidentCloze.fullText): "
-
+console.log(firstPresidentCloze.fullText)
+/*
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
 var brokenCloze = new ClozeCard("This doesn't work", "oops");
 */
