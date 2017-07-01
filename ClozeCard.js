15ad448e978297826d3616307e7b8f4c4136b63c
LmLilly <lillym@Lidiettes-MacBook-Pro.local>
2017-07-01 11:34:25 -0700

    Final

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,8 +1,22 @@
 "use strict";
 (function(){
+    function findText(fullText, cloze){
+        var findPartial = "";
+        if (fullText.indexOf(cloze) >=0){
+        //If found replace text
+            findPartial = fullText.replace(cloze, "... ");
+        }else{
+            //Not found, console not found
+            console.log(`${cloze} was not found`);
+        }
+        return findPartial
+    }
+
+
+
     function ClozeCard(fullText, cloze){
         this.cloze = cloze;
-        this.partial = fullText.replace(cloze, "... ");
+        this.partial = findText(fullText, cloze);
         this.fullText = fullText;
     }
     module.exports = ClozeCard;
--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var firstPresident = new BasicCard(
     "Who was the first president of the United States?", "George Washington");
 
 // "Who was the first president of the United States?"
-//console.log(firstPresident.front); 
+console.log(firstPresident.front); 
 
 // "George Washington"
-//console.log(firstPresident.back); 
+console.log(firstPresident.back); 
 
 var firstPresidentCloze = new ClozeCard(
     "George Washington was the first president of the United States.", "George Washington");
@@ -23,8 +23,8 @@ console.log(firstPresidentCloze.partial);
 
 // "George Washington was the first president of the United States.
 console.log(firstPresidentCloze.fullText)
-/*
+
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-var brokenCloze = new ClozeCard("This doesn't work", "oops");
-*/
+var brokenCloze = new ClozeCard("This doesn't work", "opps");
+
 })();
